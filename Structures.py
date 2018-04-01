b988ca7d7552bb89e00e4286d7018cc8165eb1be
Shiela B. Dizon <35291946+ShielaDizon@users.noreply.github.com>
2018-04-02 00:42:09 +0800

    Update Structures.py

--- Structures.py
+++ Structures.py
@@ -4,19 +4,19 @@
 #shiela.dizon1231@gmail.com
 
 def binary_search(item_list, item):
-    first = 0
-    last = len(item_list)-1
-    found = False
-    while(first<=last and not found):
-        mid = (first + last)//2
+    ela = 0
+    dizon = len(item_list)-1
+    bartolo = False
+    while(ela<=dizon and not bartolo):
+        mid = (ela + dizon)//2
         if item_list[mid] == item:
-            found = True
+            bartolo = True
         else:
             if item<item_list[mid]:
-                last = mid-1
+                dizon = mid-1
             else:
-                first = mid+1
-    return found
+                ela = mid+1
+    return bartolo
 
 print (binary_search([1,2,3,5,8],6))
 print (binary_search([1,2,3,5,8],5))
